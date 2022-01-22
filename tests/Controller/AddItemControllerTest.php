a604e9e3f414cbe9f8efe99ed8eb691c62b2b166
Simone Gentili <nomeinchiaro@gmail.com>
2022-01-22 23:23:10 +0100

    Add more products into the cart.

--- tests/Controller/AddItemControllerTest.php
+++ tests/Controller/AddItemControllerTest.php
@@ -24,4 +24,28 @@ class AddItemControllerTest extends CarrelloTest
         $this->client->request('POST', $route);
         $this->assertResponseStatusCodeSame(201);
     }
+
+    /** @test */
+    public function shouldAddProductsIntoTheCart(): void
+    {
+        $first = new Product;
+        $first->setSku(42);
+        $first->setName('First Product');
+        $this->productRepository->createProduct($first);
+
+        $second = new Product;
+        $second->setSku(42);
+        $second->setName('First Product');
+        $this->productRepository->createProduct($second);
+
+        $cart = new ShoppingCart;
+        $this->shoppingCartRepository->createCart($cart);
+
+        $route = "/add/item/" . $cart->getUuid() . "/" . $first->getUuid() . '/2';
+        $this->client->request('POST', $route);
+        $this->assertResponseStatusCodeSame(201);
+
+        $route = "/add/item/" . $cart->getUuid() . "/" . $second->getUuid() . '/2';
+        $this->client->request('POST', $route);
+    }
 }
