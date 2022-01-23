e9c95d7f7c5b49114ea9543ce447565fa0384483
Simone Gentili <nomeinchiaro@gmail.com>
2022-01-24 00:52:56 +0100

    Add more tests in AddItemControllerTest.

--- tests/Controller/AddItemControllerTest.php
+++ tests/Controller/AddItemControllerTest.php
@@ -3,6 +3,7 @@
 namespace App\Tests\Controller;
 
 use App\Entity\Product;
+use App\Entity\ProductsInCart;
 use App\Entity\ShoppingCart;
 use App\Tests\CarrelloTest;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
@@ -23,29 +24,80 @@ class AddItemControllerTest extends CarrelloTest
         $route = "/add/item/" . $cart->getUuid() . "/" . $first->getUuid() . '/2';
         $this->client->request('POST', $route);
         $this->assertResponseStatusCodeSame(201);
+        $this->assertJsonStringEqualsJsonString(
+            json_encode(['message' => 'product created']),
+            $this->client->getResponse()->getContent(),
+        );
     }
 
     /** @test */
-    public function shouldAddProductsIntoTheCart(): void
+    public function zero(): void
     {
         $first = new Product;
         $first->setSku(42);
         $first->setName('First Product');
         $this->productRepository->createProduct($first);
 
-        $second = new Product;
-        $second->setSku(43);
-        $second->setName('First Product');
-        $this->productRepository->createProduct($second);
-
         $cart = new ShoppingCart;
         $this->shoppingCartRepository->createCart($cart);
 
+        $route = "/add/item/" . $cart->getUuid() . "/" . $first->getUuid() . '/0';
+        $this->client->request('POST', $route);
+        $this->assertResponseStatusCodeSame(200);
+        $this->assertJsonStringEqualsJsonString(
+            json_encode(['message' => 'product not added']),
+            $this->client->getResponse()->getContent(),
+        );
+    }
+
+    /** @test */
+    public function shouldUpdateQuantityOfAlreadyPresentProductInCart(): void
+    {
+        $first = new Product;
+        $first->setSku(42);
+        $first->setName('First Product');
+        $this->productRepository->createProduct($first);
+
+        $cart = new ShoppingCart();
+        $this->shoppingCartRepository->createCart($cart);
+
+        $productInCart = new ProductsInCart();
+        $productInCart->setProduct($first);
+        $productInCart->setCart($cart);
+        $productInCart->setQuantity(3);
+        $this->productInCartRepository->createItem($productInCart);
+
         $route = "/add/item/" . $cart->getUuid() . "/" . $first->getUuid() . '/2';
         $this->client->request('POST', $route);
-        $this->assertResponseStatusCodeSame(201);
+        $this->assertJsonStringEqualsJsonString(
+            json_encode(['message' => 'product updated']),
+            $this->client->getResponse()->getContent(),
+        );
+    }
 
-        $route = "/add/item/" . $cart->getUuid() . "/" . $second->getUuid() . '/2';
+    /** @test */
+    public function shouldDeleteProductFromCartWheneverQuantityIsZero(): void
+    {
+        $first = new Product;
+        $first->setSku(42);
+        $first->setName('First Product');
+        $this->productRepository->createProduct($first);
+
+        $cart = new ShoppingCart();
+        $this->shoppingCartRepository->createCart($cart);
+
+        $productInCart = new ProductsInCart();
+        $productInCart->setProduct($first);
+        $productInCart->setCart($cart);
+        $productInCart->setQuantity(3);
+        $this->productInCartRepository->createItem($productInCart);
+
+        $route = "/add/item/" . $cart->getUuid() . "/" . $first->getUuid() . '/0';
         $this->client->request('POST', $route);
+        $this->assertResponseStatusCodeSame(200);
+        $this->assertJsonStringEqualsJsonString(
+            json_encode(['message' => 'product deleted']),
+            $this->client->getResponse()->getContent(),
+        );
     }
 }
